af2a232353e271f847fc7d5aa254e21071888646
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-08-26 15:52:44 +0200

    forms02

--- forms-lists-and-styling/src/App.js
+++ forms-lists-and-styling/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import InteractiveWelcome from './interactiveWelcome'
+import { Login } from './login'
 
 export class App extends React.Component {
     render() {
         return (
             <>
-                <InteractiveWelcome/>
+                <Login/>
             </>
         )    
     } 
---
+++ forms-lists-and-styling/src/login.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export class Login extends React.Component {
+    state = {
+        username: "",
+        password: "",
+        remember: false,
+        login: false
+    }
+
+    handleInput = e => {
+        // Perchè siamo costretti a definire name e ad usare [name] ? Name è forse un termine che utilizza react?
+        const name = e.target.name
+        this.setState({
+        [name]: e.target.type === "checkbox" ? e.target.checked : e.target.value,
+        })
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleInput}/><br/>
+                    <label>Password</label>
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleInput}/><br/>
+                    <label>Remember me?</label>
+                    <input type="checkbox" name="remember" value={this.state.remember} onChange={this.handleInput}/><br/><br/>
+                </div>
+            </div>
+        )
+    }
+}
