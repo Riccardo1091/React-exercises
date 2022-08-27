3fd3a8cece24ac053b99912e46c571821645e9a3
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-08-27 10:09:54 +0200

    lists04

--- forms-lists-and-styling/src/todoList.js
+++ forms-lists-and-styling/src/todoList.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {createRef} from 'react';
 
 export class TodoList extends React.Component {
-
     state = {
         items: ["Mica", "pizza", "e", "fichi", "eh"]
     }
@@ -24,19 +23,25 @@ export class TodoList extends React.Component {
     //     })
     // }
 
-    // Variante con callback "senza" return
+    // Variante con callback "senza" return e rest operator
     handleAddTodo = e => {
         e.preventDefault();
         this.setState(statoPrecedente => ({
-                items: statoPrecedente.items.concat(e.target.elements.addTodo.value)
-        }))
+                //items: statoPrecedente.items.concat(e.target.elements.addTodo.value)
+                items: [...statoPrecedente.items, e.target.elements.addTodo.value]
+        })) 
+    }
+
+    refReset = createRef();
+    componentDidUpdate() { 
+        this.refReset.current.value = '';
     }
 
     render() {
         return (
             <>     
                 <form onSubmit={this.handleAddTodo}>
-                    <input name="addTodo" type="text"/>
+                    <input name="addTodo" type="text" ref={this.refReset}/>
                     <button type="submit">Add</button>
                 </form>
                 <ul className="todo-list">
