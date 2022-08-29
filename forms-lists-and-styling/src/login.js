af3b412c98b7fba47e11d9f8981866c03476e9a0
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-08-29 23:24:14 +0200

    usestate02

--- forms-lists-and-styling/src/login.js
+++ forms-lists-and-styling/src/login.js
@@ -1,40 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class Login extends React.Component {
-    state = {
+export function Login() {
+    const [form, setForm ] = useState({
         username: "",
         password: "",
         remember: false,
         login: false
+    })
+
+    function handleInput(e) {
+        // let name = e.target.name
+        // let type = e.target.type
+        // let value = e.target.value
+        // let checked = e.target.checked
+        const {name, type, value, checked} = e.target
+        setForm(prevData => ({
+            ...prevData,
+            [name]: type === "checkbox" ? checked : value
+        }))
     }
 
-    handleInput = e => {
-        // Perchè siamo costretti a definire name e ad usare [name] ? Name è forse un termine che utilizza react?
-        const name = e.target.name
-        this.setState({
-        [name]: e.target.type === "checkbox" ? e.target.checked : e.target.value,
-        })
-    }
-
-    onLogin = e => {
-        this.setState({
+    function onLogin(e) {
+        setForm(prevData => ({
+            ...prevData,
             login: true
-        })
+        }))
     }
-
-    render() {
-        return (
-            <div className="login">
-                <div className="form-group">
-                    <label>Username</label>
-                    <input type="text" name="username" value={this.state.username} onChange={this.handleInput}/><br/>
-                    <label>Password</label>
-                    <input type="password" name="password" value={this.state.password} onChange={this.handleInput}/><br/>
-                    <label>Remember me?</label>
-                    <input type="checkbox" name="remember" value={this.state.remember} onChange={this.handleInput}/><br/><br/>
-                    {this.state.username && this.state.password ? <button type="submit" name="login" value={this.state.login} onClick={this.onLogin}>Login</button> : <button type="submit" name="login" disabled>Login</button>}
-                </div>
+    console.log(form)
+    return (
+        <div className="login">
+            <div className="form-group">
+                <label>Username</label>
+                <input type="text" name="username" value={form.username} onChange={handleInput}/><br/>
+                <label>Password</label>
+                <input type="password" name="password" value={form.password} onChange={handleInput}/><br/>
+                <label>Remember me?</label>
+                <input type="checkbox" name="remember" checked={form.remember} onChange={handleInput}/><br/><br/>
+                {form.username && form.password ? <button type="submit" name="login" value={form.login} onClick={onLogin}>Login</button> : <button type="submit" name="login" disabled>Login</button>}
             </div>
-        )
-    }
+        </div>
+    )
 }
