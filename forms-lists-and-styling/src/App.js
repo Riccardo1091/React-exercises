1fb20706be871f591c06dddd9536121a58e7596d
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-08-29 19:14:28 +0200

    function-components-02

--- forms-lists-and-styling/src/App.js
+++ forms-lists-and-styling/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Sum } from './sum'
 import { Welcome }  from './welcome'
 
 export class App extends React.Component {
     render() {
         return (
-            <Welcome name="Riccardo"/> 
+            <>
+                <Welcome name="Riccardo"/> 
+                <Sum numbers={[25,65,12,85]}/>
+            </>
         )    
     } 
 }
