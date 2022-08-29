2d6b1f180d3e588178e1311d0142b07b5d263b79
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-08-29 12:28:56 +0200

    context-01

---
+++ forms-lists-and-styling/src/displayLanguage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class DisplayLanguage extends React.Component {
+    render() {
+        return (
+            <>  
+                <p>Ciao, sono Riccardo</p>
+                <p>Hello, I'm Riccardo</p>
+            </>
+        )    
+    } 
+}
