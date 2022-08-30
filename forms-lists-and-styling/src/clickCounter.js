2d5bd78fe4164eed21498214b8d767cb8f2ab9cf
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-08-30 10:21:21 +0200

    useEffect01

--- forms-lists-and-styling/src/clickCounter.js
+++ forms-lists-and-styling/src/clickCounter.js
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react"
 
 export function ClickCounter({initialValue, increment}) {
-    const [counter, setCounter] = useState(initialValue);
+    const [counter, setCounter] = useState(initialValue)
+
+    const onCounterChange = useCallback(() => {
+        console.log(`The counter is ${counter}`)
+    }, [counter])
+
+    useEffect(() => {
+        onCounterChange()
+      }, [counter, onCounterChange])
 
     function handleCounterClick() {
         setCounter(valorePrecedente => {
             return valorePrecedente + increment;
         })
     }
-
+    
     return (
         <>  
             <h1>Click Counter</h1>
