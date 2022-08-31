4f97fd250e0ac799bc21d51cf9364ef83ac8d16f
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-08-31 16:15:40 +0200

    customHooks04

--- forms-lists-and-styling/src/githubUser.js
+++ forms-lists-and-styling/src/githubUser.js
@@ -1,12 +1,12 @@
 import { UseGithubUser } from "./useGithubUser"
 
 export function GithubUser({username}) {
-    const {utente} = UseGithubUser(username)
+    const {utente, error, loading} = UseGithubUser(username)
 
     return (
         <>  
-            {utente.login ? <h1>The username is: {utente.login}</h1> : <h1>Fetching Username....</h1>}
-            {utente.bio ? <h3>Bio: {utente.bio}</h3> : <h3>Fetching Bio....</h3>}
+            {error ? "Ci sono dei problemi" : loading ? <h1>Caricamento...</h1> : <h1>The username is: {utente.login}</h1>}
+            {error ? "Ci sono dei problemi" : loading ? <h1>Caricamento...</h1> : <h3>Bio: {utente.bio}</h3>}
         </>
     )
 }
--- forms-lists-and-styling/src/useGithubUser.js
+++ forms-lists-and-styling/src/useGithubUser.js
@@ -2,16 +2,25 @@ import { useState, useEffect } from "react"
 
 export function UseGithubUser(username) {
     const [utente, setUtente] = useState(username)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         (async () => {
+            setLoading(true)
             const res = await fetch(`https://api.github.com/users/${username}`)
-            const data = await res.json()
-            setUtente(data) // punto chiave
-        })();    
-    }, [username]);
+            if (!res.ok) {
+                setError(true)
+                setLoading(false)
+            } else {
+                const data = await res.json()
+                setUtente(data) // punto chiave
+            }
+            setLoading(false)
+        })()
+    }, [username])
 
     return {
-        utente: utente
+        utente, error, loading
     }
 }
