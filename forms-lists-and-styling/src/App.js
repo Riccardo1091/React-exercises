19acb6ca83b42b718233830bbe452e3b6ee370d9
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-09-02 14:54:31 +0200

    router07

--- forms-lists-and-styling/src/App.js
+++ forms-lists-and-styling/src/App.js
@@ -12,7 +12,7 @@ export function App() {
                 <div className="inline-flex rounded-md shadow-sm">
                     <Link aria-current="page" className="py-2 px-4 text-sm font-medium text-blue-700 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white" to="/">Home</Link>
                     <Link className="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white" to="/counter">Counter</Link>
-                    <Link className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white" to="/users/riccardo1091">Username</Link>
+                    <Link className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white" to="/users">Users</Link>
                 </div><br/>
                 <Routes>
                     <Route
@@ -27,6 +27,7 @@ export function App() {
                     <Route path='/' element={<Welcome name="Riccardo"/>} />
                     <Route path='/counter' element={<ClickCounter initialValue={5} increment={5}/>} />
                     <Route path='/users' element={<div><GithubUserList usernames={["Riccardo1091", "MAugelli", "msimile", "rickcrypto90"]}/><hr/><Outlet/></div>}>
+                        <Route index element={<div><h1>Add a user and select it</h1></div>} />
                         <Route path=':username' element={<ShowGithubUser/>} />
                     </Route>
                     {/* <Route path='/users/:username' element={<ShowGithubUser/>} />  */}
