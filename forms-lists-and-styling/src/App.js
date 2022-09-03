c1059c8e5c741aab9d98b181be6ff2c137ea1030
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-09-03 14:35:14 +0200

    swr04

--- forms-lists-and-styling/src/App.js
+++ forms-lists-and-styling/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { SWRConfig } from 'swr'
 import { GithubUser } from './githubUser'
+
+const fetcher = async username => {
+    const res = await fetch(`https://api.github.com/users/${username}`)
+    if (!res.ok) {
+        throw Error('Errore nel fetch')
+    } else {return await res.json()}  
+}
 
 export function App() {
     return (
         <>  
-            <GithubUser username="riccardo1091"/>  
+            <SWRConfig value={{fetcher}}>
+                <GithubUser username="riccardo1091"/>
+            </SWRConfig>
         </>
     )    
 }
--- forms-lists-and-styling/src/useGithubUser.js
+++ forms-lists-and-styling/src/useGithubUser.js
@@ -1,17 +1,18 @@
 import useSWR from 'swr'
 
-const fetcher = async username => {
-    const res = await fetch(username)
-    if (!res.ok) {
-        const error = new Error('Errore nel fetch').message
-        error.info = await res.json()
-        error.status = res.status
-        throw error
-    } else {return await res.json()}  
-}
+// const fetcher = async username => {
+//     const res = await fetch(username)
+//     if (!res.ok) {
+//         const error = new Error('Errore nel fetch').message
+//         error.info = await res.json()
+//         error.status = res.status
+//         throw error
+//     } else {return await res.json()}  
+// }
 
 export function UseGithubUser(username) {
-    const { data, error, mutate } = useSWR(() => username ? `https://api.github.com/users/${username}`: null, fetcher)
+    const { data, error, mutate } = useSWR(username ? `https://api.github.com/users/${username}`: null)
+    
 
     function reFetchData(){
         mutate()
